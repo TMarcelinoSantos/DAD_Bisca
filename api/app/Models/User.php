f7a6b237f7877a278b986e0f14eb8b8780835d87
TomasMSantos <tomasmarcelinosantos@gmail.com>
2025-11-23 21:53:42 +0000

    Login in API

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -6,11 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +23,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'photo_url',
     ];
 
     /**
@@ -45,4 +48,5 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/users/me', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+    });
+    Route::post('logout', [AuthController::class, 'logout']);
+    });
 
 Route::get('/metadata', function (Request $request) {
     //abort(500, 'Something went wrong');
